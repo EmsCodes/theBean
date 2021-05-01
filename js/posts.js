2b626f5bf40a26ab86e8fad7068d9f3e48c4721d
EmsCodes <69721898+EmsCodes@users.noreply.github.com>
2021-05-01 20:16:29 +0200

    REST API
    test

---
+++ js/posts.js
@@ -0,0 +1,45 @@
+const url = "https://makra-stenkloev.no/thebean/wp-json/wp/v2/posts?_embed";
+
+const blogPostsContainer = document.querySelector(".blog-list-container");
+
+
+async function fetchPosts(){
+
+    try{
+        const response = await fetch(url);
+
+        const json = await response.json();
+
+        const posts = json;
+
+        console.log(json);
+
+        for(let i = 0; i < posts.length; i++){
+
+            const featuredImage = posts[i]._embedded["wp:featuredmedia"][0].source_url;
+            const altImageText = posts[i]._embedded["wp:featuredmedia"][0].alt_text;
+
+            // for posts.html
+            
+            blogPostsContainer.innerHTML += 
+            `<a href="specific-post.html?id="${posts[i].id}">
+                <div class="post-container">
+                    <div class="featured-image" style="background-image:url(${featuredImage})" alt="${altImageText}"></div>
+                    <div class="container-text-width">
+                        <h4>${posts[i].title.rendered}</h4>
+                        <p class="date">(${posts[i].date})</p>
+                        <p class="description">${posts[i].excerpt.rendered}</p>
+                        <p class="read-link">read></p>
+                    </div>
+                </div>
+            </a>`;
+            }
+        
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+}
+
+fetchPosts();
--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ const url = "https://makra-stenkloev.no/thebean/wp-json/wp/v2/posts?_embed";
 
 const carouselContainer = document.querySelector("#posts-carousel");
 
-// const blogPostsContainer = document.querySelector(".posts-container");
-
-
 async function fetchPosts(){
 
     try{
@@ -17,14 +14,11 @@ async function fetchPosts(){
         console.log(json);
 
         carouselContainer.innerHTML = "";
-        // blogPostsContainer.innerHTML = "";
 
         for(let i = 0; i < posts.length; i++){
 
             const featuredImage = posts[i]._embedded["wp:featuredmedia"][0].source_url;
             const altImageText = posts[i]._embedded["wp:featuredmedia"][0].alt_text;
-
-            console.log(featuredImage);
 
             // for index.html
             
@@ -36,17 +30,6 @@ async function fetchPosts(){
                     <p>read></p>
                 </div>
             </a>`;
-
-            // for posts.html
-
-            // blogPostsContainer.innerHTML += 
-            // `<a href="specific-post.html?id="${posts[i].id}">
-            //     <div class="post-container">
-            //         <div class="featured-image" style="background-image:url(${featuredImage})" alt="${altImageText}"></div>
-            //         <h4>${posts[i].title.rendered}</h4>
-            //         <p>read></p>
-            //     </div>
-            // </a>`;
         }
         
     }
@@ -58,3 +41,25 @@ async function fetchPosts(){
 
 fetchPosts();
 
+
+// function blogListHtml(posts){
+
+//     for(let i = 0; i < posts.length; i++){
+
+//         const featuredImage = posts[i]._embedded["wp:featuredmedia"][0].source_url;
+//         const altImageText = posts[i]._embedded["wp:featuredmedia"][0].alt_text;
+
+//         console.log(featuredImage);
+
+//         // for posts.html
+
+//         blogPostsContainer.innerHTML += 
+//         `<a href="specific-post.html?id="${posts[i].id}">
+//             <div class="list-container">
+//                 <div class="list-featured-image" style="background-image:url(${featuredImage})" alt="${altImageText}"></div>
+//                 <h4>${posts[i].title.rendered}</h4>
+//                 <p>read></p>
+//             </div>
+//         </a>`;
+//     }
+// }
